acf7c1b0c01f053468e80917f72adf93a2f6b0dd
Johnny Galindo <John_galindo@hotmail.com>
2021-01-05 14:46:32 -0600

    initial

--- AzureStorageTest/Form1.cs
+++ AzureStorageTest/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Azure.Storage.Blobs;
+using Microsoft.Azure; //Namespace for CloudConfigurationManager
+
 
 namespace AzureStorageTest
 {
@@ -15,6 +18,39 @@ namespace AzureStorageTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = textBox3.Text;
+
+            string downloadpath = textBox4.Text;
+
+            downloadpath = downloadpath.Replace(@"\", @"\\");
+            lblmessage.Text = "";
+            try
+            {
+                Azure.Storage.Blobs.BlobContainerClient container = new Azure.Storage.Blobs.BlobContainerClient(textBox1.Text, textBox2.Text);
+
+
+                var BlobClient = container.GetBlobClient(textBox3.Text);
+                BlobClient.DownloadTo(downloadpath + "\\" + file);
+                lblmessage.Text= "Downloaded file to "+ textBox4.Text + @"\" +file+" Sucessfully";
+            }
+            catch(Exception ex)
+            {
+                lblmessage.Text = ex.Message.ToString();
+
+            }
+            
+          
+
         }
     }
 }
